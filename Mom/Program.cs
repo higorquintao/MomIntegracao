42d2c1e99ba947987712582fdb379206c6e4ea40
Higor André Quintão <higorquintao@gmail.com>
2015-06-21 22:00:11 -0300

    Ultimo commit de hoje, somente alterando o probrama que incia o Mom. De Windows forms para console.

--- Mom/Program.cs
+++ Mom/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MomCommon;
 namespace Mom
 {
     static class Program
@@ -14,9 +14,18 @@ namespace Mom
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            try {
+                Console.WriteLine("Inciando MOM.");
+                Run.Start();
+                Console.WriteLine("MOM Iniciado com sucesso.");
+                while (true);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
